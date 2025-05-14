c34e6bb20b71c5d8d76dbfe5336f82487c1a1188
tebibu12-lab <stebibu42@gmail.com>
2025-05-14 22:03:15 +0300

    search logic

--- Src/Search.py
+++ Src/Search.py
@@ -1,1 +1,8 @@
+import json
+
+def search_medicine(query):
+    with open("data/pharmacy_data.json", "r") as f:
+        data = json.load(f)
+    matches = [m for m in data if query.lower() in m['name'].lower()]
+    return matches
 
