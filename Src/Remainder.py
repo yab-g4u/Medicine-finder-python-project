3462f586d680284fd1bbea1a9c04e86a3e9a3592
yaredmihretthe1st <mihretyared14@gmail.com>
2025-05-14 21:30:36 +0300

    Remainder logic

--- Src/Remainder.py
+++ Src/Remainder.py
@@ -1,1 +1,8 @@
+import json
+import datetime
 
+def check_reminders():
+    now = datetime.datetime.now().strftime("%H:%M")
+    with open("data/prescription.json", "r") as f:
+        prescriptions = json.load(f)
+    return [p for p in prescriptions if p['time'] == now]
